7befd1c5497c3bd417790aed71c392d266150b91
Francois Breard <francois.breard@systrangroup.com>
2016-02-18 17:25:21 +0000

    fix folder name in the no apiKey.txt error message.

--- SystranClientNlpApiLibTests/Api/NerApiTests.cs
+++ SystranClientNlpApiLibTests/Api/NerApiTests.cs
@@ -25,7 +25,7 @@ namespace Systran.NlpClientLib.Api.Tests
             client = new ApiClient("https://platform.systran.net:8904");
             Configuration.apiClient = client;
             if (!File.Exists("../../apiKey.txt"))
-                throw new Exception("To properly run the tests, please add an apiKey.txt file containing your api key in the SystranClientMultimodalApiLibTests folder or edit the test file with your key");
+                throw new Exception("To properly run the tests, please add an apiKey.txt file containing your api key in the SystranClientNlpApiLibTests folder or edit the test file with your key");
             Dictionary<String, String> keys = new Dictionary<String, String>();
             string key;
             using (StreamReader streamReader = new StreamReader("../../apiKey.txt", Encoding.UTF8))
